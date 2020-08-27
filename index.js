3e250ebaed580f2d452e064202a961ce250ebc42
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-08-27 23:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function map(src, callBack) {
   let r = []
   for (let i = 0; i < src.length; i++) {
     let theElement = src[i]
-    r.push(cb(theElement))
+    r.push(callBack(theElement))
   }
   return r;
 }
 
-function reduce(src, cb, starting){
+function reduce(src, callBack, starting){
   let r = (!!starting) ? starting : src[0]
   let i = (!!starting) ? 0 : 1
 
   for (; i < src.length; i++) {
-    r = cb(src[i], r)
+    r = callBack(src[i], r)
   }
   return r;
 }
